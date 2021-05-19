c3ccf3393fcd0e9c393a72154657d43a9431140c
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-05-19 13:58:37 +0200

    disgne effectuer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
